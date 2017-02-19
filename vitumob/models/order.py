48ad0e7495770156fecfe28650f45bd4c3613087
Eugene Mutai <eugenemutai@gmail.com>
2017-02-19 17:25:20 +0300

    Removed commented code

--- vitumob/models/order.py
+++ vitumob/models/order.py
@@ -7,17 +7,3 @@ class Order(ndb.Expando):
     items = ndb.StructuredProperty(Item, repeated=True)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
-
-# class Order(ndb.Expando):
-#     uuid = ndb.StringProperty()
-#     location = ndb.StringProperty()
-#     amount = ndb.FloatProperty()
-#     total = ndb.FloatProperty()
-#     exchange_rate = ndb.FloatProperty()
-#     customs = ndb.FloatProperty()
-#     shipping_cost = ndb.FloatProperty()
-#     vat = ndb.FloatProperty()
-#     coupon = ndb.StringProperty()
-#     items = ndb.StructuredProperty(Item, repeated=True)
-#     created_at = ndb.DateTimeProperty(auto_now_add=True)
-#     updated_at = ndb.DateTimeProperty(auto_now=True)
