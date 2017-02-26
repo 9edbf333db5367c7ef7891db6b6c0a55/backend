134d188fd0b6af33c065652a113d1f576c32f002
Eugene Mutai <eugenemutai@gmail.com>
2017-02-26 13:04:17 +0300

    Add ShippingInfo model mapping for some items

--- vitumob/models/item.py
+++ vitumob/models/item.py
@@ -1,5 +1,15 @@
 from google.appengine.ext import ndb
 
+
+class ShippingInfo(ndb.Expando):
+    length = ndb.FloatProperty()
+    height = ndb.FloatProperty()
+    width = ndb.FloatProperty()
+    weight = ndb.FloatProperty()
+    shipping_cost = ndb.FloatProperty()
+    is_prime_item = ndb.BooleanProperty()
+
+
 class Item(ndb.Expando):
     id = ndb.StringProperty()
     name = ndb.StringProperty()
@@ -7,6 +17,8 @@ class Item(ndb.Expando):
     link = ndb.StringProperty()
     quantity = ndb.IntegerProperty()
     price = ndb.FloatProperty()
+    shipping_info = ndb.StructuredProperty(ShippingInfo)
+    total_cost = ndb.FloatProperty()
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
-    # Others - size, color
+    # Others - size, color, asin, model_no
