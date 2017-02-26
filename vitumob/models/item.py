abbdb6ac5a660b9eb6db24b4e86b8228acbdc6e0
Eugene Mutai <eugenemutai@gmail.com>
2017-02-26 18:04:28 +0300

    Add order & item total, shipping and overall cost

--- vitumob/models/item.py
+++ vitumob/models/item.py
@@ -1,12 +1,12 @@
 from google.appengine.ext import ndb
 
 
-class ShippingInfo(ndb.Expando):
+class ShippingInfo(ndb.Model):
     length = ndb.FloatProperty()
     height = ndb.FloatProperty()
     width = ndb.FloatProperty()
     weight = ndb.FloatProperty()
-    shipping_cost = ndb.FloatProperty()
+    shipping_cost = ndb.FloatProperty(default=0.00)
     is_prime_item = ndb.BooleanProperty()
 
 
@@ -15,10 +15,12 @@ class Item(ndb.Expando):
     name = ndb.StringProperty()
     image = ndb.StringProperty()
     link = ndb.StringProperty()
-    quantity = ndb.IntegerProperty()
-    price = ndb.FloatProperty()
+    quantity = ndb.IntegerProperty(default=1)
+    price = ndb.FloatProperty(default=0.00)
+    total_cost = ndb.FloatProperty(default=0.00)
+    shipping_cost = ndb.FloatProperty(default=0.00)
+    overall_cost = ndb.ComputedProperty(lambda self: self.total_cost + self.shipping_cost)
     shipping_info = ndb.StructuredProperty(ShippingInfo)
-    total_cost = ndb.FloatProperty()
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
     # Others - size, color, asin, model_no
