ba2f6b0cd7ee6cfd71a78c9f5b0e7ae3a167dfa8
Eugene Mutai <eugenemutai@gmail.com>
2017-02-26 15:55:01 +0300

    If orders are from amazon, get items shipping details

--- vitumob/models/order.py
+++ vitumob/models/order.py
@@ -3,9 +3,9 @@ from .item import Item
 
 
 class Order(ndb.Model):
-    name = ndb.StringProperty()
-    belong_to = ndb.StringProperty()
-    host = ndb.StringProperty()
+    uuid = ndb.StringProperty()
+    # belongs_to = ndb.StringProperty()
+    merchant = ndb.StringProperty()
     items = ndb.KeyProperty(kind=Item, repeated=True)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
