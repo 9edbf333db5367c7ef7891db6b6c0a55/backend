7f76cfb46c571dba6b81bfed15964d1fcd70ff3e
Eugene Mutai <eugenemutai@gmail.com>
2017-02-26 18:04:28 +0300

    Add order & item total, shipping and overall cost

--- vitumob/controllers/orders.py
+++ vitumob/controllers/orders.py
@@ -2,12 +2,14 @@
 
 import json
 import logging
+
+from functools import reduce
 from flask import Blueprint, Response, request
 from google.appengine.ext import ndb
 
-from ..models.order import Order
 from ..models.item import Item, ShippingInfo
-from ..utils import ndb_json
+from ..models.order import Order
+# from ..utils import ndb_json
 from ..utils.shipping.amazon import AmazonShippingCost
 
 
@@ -32,24 +34,51 @@ def new_order_from_extension():
                 if len(shipping_info) > 0:
                     shipping_info = shipping_info[0]
                     item['name'] = shipping_info['title']
-                    item['total_cost'] = item['price'] + \
-                        shipping_info['shipping_cost']
+                    item['total_cost'] = item['price'] * item['quantity']
+                    item['shipping_cost'] = shipping_info['shipping_cost'] * item['quantity']
 
                     shipping_info.pop('asin', None)
                     shipping_info.pop('title', None)
                     item['shipping_info'] = ShippingInfo(**shipping_info)
                 else:
-                    logging.debug("No shipping information was captured for %s", item['name'])
+                    logging.debug(
+                        "No shipping information was captured for %s",
+                        item['name']
+                    )
 
                 new_order['items'][index] = item
 
+    # if items not from amazon
+    else:
+        def return_with_costs(item):
+            item['total_cost'] = item['price'] * item['quantity']
+            item['shipping_cost'] = item['quantity'] * (2.20462 * 7.50)
+            return item
+        new_order['items'] = map(return_with_costs, new_order['items'])
+
+    # calculate the order's total shipping cost
+    item_shipping_costs = [item['shipping_cost'] for item in new_order['items']]
+    new_order['shipping_cost'] = reduce(lambda a, b: a + b, item_shipping_costs, 0.00)
+
+    # calculate the order's total item costs
+    costs_of_items = [item['total_cost'] for item in new_order['items']]
+    new_order['total_cost'] = reduce(lambda a, b: a + b, costs_of_items, 0.00)
+
     # store the items 1st, collecting their DB keys
     items = [Item(**item) for item in new_order['items']]
-    item_keys = [item.put() for item in items]
+    item_keys = ndb.put_multi(items)
 
     # now store the order, referencing the keys to the items of order
-    order = Order(merchant=new_order['merchant'], uuid=new_order['uuid'], items=item_keys)
-    order_key = order.put() # Order.query(Order.key == order_key).get()
+    new_order.pop('items', item_keys)
+    order = Order(**new_order)
+    order_key = order.put()  # Order.query(Order.key == order_key).get()
 
-    payload = json.dumps({'orderId': order_key.urlsafe() })
+    order_info = order_key.get()
+    payload = json.dumps({
+        'order_id': order_key.id(),
+        'order_hex': order_key.urlsafe(),
+        'overall_cost': order_info.overall_cost,
+        'markup': (order_info.overall_cost / order_info.total_cost) - 1,
+        'shipping_cost': order_info.shipping_cost
+    })
     return Response(payload, status=200, mimetype='application/json')
--- vitumob/models/order.py
+++ vitumob/models/order.py
@@ -7,5 +7,8 @@ class Order(ndb.Model):
     # belongs_to = ndb.StringProperty()
     merchant = ndb.StringProperty()
     items = ndb.KeyProperty(kind=Item, repeated=True)
+    total_cost = ndb.FloatProperty(default=0.00)
+    shipping_cost = ndb.FloatProperty(default=0.00)
+    overall_cost = ndb.ComputedProperty(lambda self: self.total_cost + self.shipping_cost)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
