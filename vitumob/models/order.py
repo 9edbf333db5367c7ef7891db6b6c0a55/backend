05c3856388ee1a39dba1d13b42ea80069a431c26
Eugene Mutai <eugenemutai@gmail.com>
2017-02-27 14:19:36 +0300

    Add vat & customs to order being stored & response

--- vitumob/models/order.py
+++ vitumob/models/order.py
@@ -9,6 +9,20 @@ class Order(ndb.Model):
     items = ndb.KeyProperty(kind=Item, repeated=True)
     total_cost = ndb.FloatProperty(default=0.00)
     shipping_cost = ndb.FloatProperty(default=0.00)
-    overall_cost = ndb.ComputedProperty(lambda self: self.total_cost + self.shipping_cost)
+    customs = ndb.ComputedProperty(lambda self: self.total_cost * 0.12)
+    vat = ndb.ComputedProperty(lambda self: self.total_cost * 0.16)
+    overall_cost = ndb.FloatProperty()
+    markup = ndb.FloatProperty()
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
+
+    def _pre_put_hook(self):
+        self.overall_cost = reduce(lambda a, b: a + b, [
+            self.total_cost,
+            self.shipping_cost,
+            self.customs,
+            self.vat
+        ], 0.00)
+
+        self.markup = (self.overall_cost / self.total_cost) - 1
+        self.markup = float(format(self.markup, ".2f"))
