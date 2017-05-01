b9109b00eb292b87de5f652ba4e093e98c9b4a4a
Eugene Mutai <eugenemutai@gmail.com>
2017-05-01 21:20:23 +0300

    Enabled CORS

--- vitumob/__init__.py
+++ vitumob/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask
+from flask_cors import CORS, cross_origin
+
 from .controllers.orders import orders
 from .controllers.cart import cart
 from .controllers.coupons import coupons
+from .controllers.rates import exchangerates
+from .controllers.payments import payments
+
 
 app = Flask(__name__)
-app.register_blueprint(orders)
-app.register_blueprint(cart)
-app.register_blueprint(coupons)
+CORS(app)
+
+resources = [orders, cart, coupons, exchangerates, payments]
+for resource in resources:
+    app.register_blueprint(resource)
