531d5d960f4e1996cf48c8f49ef452a2f3b73340
Eugene Mutai <eugenemutai@gmail.com>
2018-10-24 20:44:52 +0300

    Add MPESA payment integration endpoints and business logic

--- vitumob/__init__.py
+++ vitumob/__init__.py
@@ -8,13 +8,14 @@ from .controllers.cart import cart
 from .controllers.payments import payments
 from .controllers.mpesa import mpesa
 from .controllers.rates import exchangerates
-from .controllers.coupons import coupons
+# from .controllers.coupons import coupons
+from .controllers.mpesa_push_api import mpesa_push_api
 
 
 app = Flask(__name__)
 app.secret_key = os.urandom(18)
 CORS(app)
 
-resources = [user, orders, cart, payments, mpesa, exchangerates, coupons]
+resources = [user, orders, cart, payments, mpesa, exchangerates, mpesa_push_api]
 for resource in resources:
     app.register_blueprint(resource)
--- vitumob/models/mpesa.py
+++ vitumob/models/mpesa.py
@@ -3,23 +3,29 @@ from datetime import datetime
 from google.appengine.ext import ndb
 
 
-class MpesaCredentials(ndb.Expando):
-    access_token = ndb.StringProperty()
-    expires_in = ndb.FloatProperty(default=0.00)
+class MpesaDarajaAccessToken(ndb.Expando):
+    access_token = ndb.StringProperty(default='', required=True)
+    expires_in = ndb.IntegerProperty(default=0, required=True)
     expiring_time = ndb.FloatProperty(default=0.00)
     updated_at = ndb.DateTimeProperty(auto_now=True)
 
     def _pre_put_hook(self):
-        self.expiring_time = time.mktime(datetime.now().timetuple()) + self.expires_in
+        time_now_in_seconds = time.mktime(datetime.now().timetuple())
+        self.expiring_time = time_now_in_seconds + self.expires_in
 
 
 class MpesaPayment(ndb.Expando):
+    order_id = ndb.StringProperty()
+    user_id = ndb.StringProperty()
+    merchant_request_id = ndb.StringProperty(default='')
+    checkout_request_id = ndb.StringProperty(default='')
     # id = ndb.GenericProperty()
     code = ndb.StringProperty()
     phone_no = ndb.StringProperty()
-    sender = ndb.StringProperty()
+    sender = ndb.StringProperty(default='')
     # recoded_trx_date_time = ndb.DateTimeProperty()
     amount = ndb.FloatProperty(default=0.00)
+    # transaction_date = ndb.IntegerProperty(default=0)
     created_at = ndb.DateTimeProperty(auto_now_add=True)
     updated_at = ndb.DateTimeProperty(auto_now=True)
 
